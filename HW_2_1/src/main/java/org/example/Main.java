996ff979fb90ab4404c480688fa293d84a330f05
lenok1998 <lena.malicheva@mail.ru>
2023-10-23 23:38:18 +0400

    HW_2

--- HW_2_1/src/main/java/org/example/Main.java
+++ HW_2_1/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Здравствуйте!");
+        System.out.println("Добро пожаловать!");
     }
 
 
